975a339f0f21e7fe91aaadce7c2075509d7e49b0
Florian Reisecker <florian.reisecker@gmail.com>
2015-08-15 13:18:43 +0200

    add exception handling to sensor_reader.py

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RaspTemper
 Reads temperature data from Raspberry Pi2, pushed it to a server and makes it accessibly by an web application
 ## Sensor reader
-TODO documentation (setup python libaries for specific sensor, setup request libary, setup crontab)
+TODO offline caching (broken!), documentation (setup python libaries for specific sensor, setup request libary, setup crontab)
 ## Data Server
 TODO documentation
 ## Web application
--- sensor-reader/sensor_reader.py
+++ sensor-reader/sensor_reader.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 
 import requests
 import datetime
 import json
 import subprocess
+import sys
 import os
 
 # constants
 PATH_TO_DATA_PERSISTANT = 'data_persistent.txt'
 PATH_TO_DATA_UNSENT = 'data_unsent.txt'
 TEMPERATURE_READ_COMMAND = '/usr/local/bin/temper-poll -q -c'
-SERVER_URL = 'http://www.y o u r _ s e r v e r/update'
-HEADERS = {'Content-Type': 'application/json;charset=UTF-8'}
+SERVER_URL = 'YOUR_SERVER'
+HEADER = {'Content-Type': 'application/json;charset=UTF-8'}
 
-# create temp file if it does not exist yet
+# create temp file if needed
 if not os.path.isfile(PATH_TO_DATA_UNSENT):
     open(PATH_TO_DATA_UNSENT,'a').write('')
 
@@ -25,21 +26,24 @@ timestamp = datetime.datetime.now().isoformat()
 humidity = 0
 sensordata = [{'timestamp': timestamp, 'temperature': temperature, 'humidity': humidity}]
 
-# save new data to local file
+# save new data to both persistant and temp file
 for item in sensordata:
-      open(PATH_TO_DATA_PERSISTANT, 'a').write('%s\n' % item)
+    open(PATH_TO_DATA_PERSISTANT, 'a').write('%s\n' % item)
+    open(PATH_TO_DATA_UNSENT, 'a').write('%s\n' % item)
 
 # add unsent data if any
 data_unsent = [line.strip() for line in open(PATH_TO_DATA_UNSENT, 'r')]
 data_to_send = sensordata if not data_unsent else data_unsent+sensordata
 
-# send data
-response_code = requests.post(SERVER_URL, data=json.dumps(sensordata), HEADERS=HEADERS).status_code
-
-# check request code: if save on server failed, write it to file and try again next time
-if response_code == 200:
-    open(PATH_TO_DATA_UNSENT, 'w').write('');
-else:
-    for item in sensordata:
-      open(PATH_TO_DATA_UNSENT, 'a').write('%s\n' % item)
-print('Server response: ' + str(response_code))
+# send data and check request code: if sending to server failed, write it to file and try again next time
+try:
+    print('Trying to send %i sensor reading(s)...' % len(data_unsent))
+    response_code = requests.post(SERVER_URL, data=json.dumps(sensordata), headers=HEADER).status_code
+    if response_code == 200:
+        # SUCCESS remove sent data from temp file
+        open(PATH_TO_DATA_UNSENT, 'w').write('');
+        print('SUCCESS: Server response: ' + str(response_code))
+    else:
+        print('FAILURE: Server response: ' + str(response_code))
+except:
+    print('FAILURE: Unexpected error:', sys.exc_info()[0])
